42526ef5a911e098ff9cb7dde5282ddf13846ff7
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-10-22 23:55:35 +0300

    Add files via upload

---
+++ TasksCycle.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A, counter = 0;
+	cin >> A;
+
+	while (A > 0)
+	{
+		A = A / 10;
+		counter++;
+	}
+	cout << counter;
+
+	system("pause");
+}
